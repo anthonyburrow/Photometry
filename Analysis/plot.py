e4adaab007fcd217ed744491d13ec8f3df893f35
Anthony Burrow <anthony.r.burrow@gmail.com>
2018-11-12 19:57:16 -0600

    Made plotting more standardized

--- Analysis/plot.py
+++ Analysis/plot.py
@@ -20,67 +20,11 @@ def ProcessPlot(cluster, date, app):
         filename = path + 'beList_scaled_' + app.phot_type + file_type + '.dat'
         filtered_data = np.loadtxt(filename, ndmin=2)
 
-        ColorMagnitudeDiagram(cluster, date, app, data, filtered_data, file_type)
-        TwoColorDiagram(cluster, date, app, data, filtered_data, file_type)
+        SinglePlot(cluster, date, app, data, filtered_data, file_type, '2cd')
+        SinglePlot(cluster, date, app, data, filtered_data, file_type, 'cmd')
 
 
-def ColorMagnitudeDiagram(cluster, date, app, data, filtered_data, file_type):
-    """Specifies the plotting of a color magnitude diagram.
-
-    Calls to plot a color magnitude diagram with V vs. B-V axes.
-
-    """
-    print("  Creating color-magnitude diagram...")
-
-    # Plot full data
-    B_V = data[:, 2] - data[:, 4]
-    B_Verr = np.sqrt(data[:, 3]**2 + data[:, 5]**2)
-
-    try:
-        filtered_B_V = filtered_data[:, 2] - filtered_data[:, 4]
-        filtered_V = filtered_data[:, 4]
-    except IndexError:
-        filtered_B_V = []
-        filtered_V = []
-
-    title = 'V vs. B-V'
-    if file_type == '_lowError':
-        title += ' (Low Error)'
-    SinglePlot(cluster, date, app, B_V, data[:, 4], title, 'B-V', 'V',
-               B_Verr, data[:, 5], filtered_B_V, filtered_V,
-               'CMD_' + app.phot_type + file_type + '.png')
-
-
-def TwoColorDiagram(cluster, date, app, data, filtered_data, file_type):
-    """Specifies the plotting of a two-color diagram.
-
-    Calls to plot a color magnitude diagram with R-H vs. B-V axes.
-
-    """
-    print("  Creating two-color diagram...")
-
-    # Plot full data
-    B_V = data[:, 2] - data[:, 4]
-    B_Verr = np.sqrt(data[:, 3]**2 + data[:, 5]**2)
-    R_H = data[:, 6] - data[:, 8]
-    R_Herr = np.sqrt(data[:, 7]**2 + data[:, 9]**2)
-
-    try:
-        filtered_B_V = filtered_data[:, 2] - filtered_data[:, 4]
-        filtered_R_H = filtered_data[:, 6] - filtered_data[:, 8]
-    except IndexError:
-        filtered_B_V = []
-        filtered_R_H = []
-
-    title = 'R-Halpha vs. B-V'
-    if file_type == '_lowError':
-        title += ' (Low Error)'
-    SinglePlot(cluster, date, app, B_V, R_H, title, 'B-V', 'R-Halpha',
-               B_Verr, R_Herr, filtered_B_V, filtered_R_H,
-               '2CD_' + app.phot_type + file_type)
-
-
-def SinglePlot(cluster, date, app, x, y, title, x_label, y_label, x_err, y_err, be_x, be_y, output):
+def SinglePlot(cluster, date, app, data, filtered_data, file_type, plot_type):
     """Creates a single plot of given data.
 
     General configuration of plotting style and other specifications, including data,
@@ -88,85 +32,98 @@ def SinglePlot(cluster, date, app, x, y, title, x_label, y_label, x_err, y_err, 
     directory.
 
     Args:
-            x: The x-axis array to be plotted.
-            y: The y-axis array to be plotted.
-            title: The title of the plot.
-            x_label: The label of the x-axis.
-            y_label: The label of the y-axis.
-            x_err: The x-axis error information.
-            y_err: The y-axis error information.
-            be_x: The x-axis Be-filtered data.
-            be_y:The y-axis Be-filtered data.
-            output: The filename desired for the plot.
 
     """
-    # plt.style.use('ggplot')
+    # Setup plot items
+    if plot_type == '2cd':
+        y = data[:, 6] - data[:, 8]
+        y_err = np.sqrt(data[:, 7]**2 + data[:, 9]**2)
+        be_y = filtered_data[:, 6] - filtered_data[:, 8]
 
-    plt.figure(figsize=(12, 9))
+        title = 'R-Halpha vs. B-V'
+        y_label = 'R-Halpha'
+        output = '2CD_' + app.phot_type + file_type + '.png'
+    elif plot_type == 'cmd':
+        y = data[:, 4]
+        y_err = data[:, 5]
+        be_y = filtered_data[:, 4]
+
+        title = 'V vs. B-V'
+        y_label = 'V'
+        output = 'CMD_' + app.phot_type + file_type + '.png'
+
+    # try:
+    #     filtered_B_V = filtered_data[:, 2] - filtered_data[:, 4]
+    #     filtered_V = filtered_data[:, 4]
+    # except IndexError:
+    #     filtered_B_V = []
+    #     filtered_V = []
+
+    x = data[:, 2] - data[:, 4]
+    x_err = np.sqrt(data[:, 3]**2 + data[:, 5]**2)
+    be_x = filtered_data[:, 2] - filtered_data[:, 4]
+
+    if file_type == '_lowError':
+        title += ' (Low Error)'
+    x_label = 'B-V'
+
+    # Create plot
+    plt.style.use('researchpaper')
+    fig, ax = plt.subplots()
 
     # Plot main data
-    plt.plot(x, y, 'o', color='#3f3f3f', markersize=12)
-    # plt.title(title)
-    plt.xlabel(x_label, fontsize=36)
-    plt.ylabel(y_label, fontsize=36)
+    ax.plot(x, y, 'o', color='#3f3f3f', markersize=12)
+    # plt.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
 
-    plt.axes().xaxis.set_major_locator(MultipleLocator(1))
-    plt.axes().xaxis.set_major_formatter(FormatStrFormatter('%d'))
-    plt.axes().xaxis.set_minor_locator(MultipleLocator(0.25))
+    ax.set_xlim([app.B_VMin - 0.1, app.B_VMax + 2.5])
+    ax.set_ylim([18.5 - app.A_v, 8.5 - app.A_v])
 
-    plt.axes().yaxis.set_major_locator(MultipleLocator(2))
-    plt.axes().yaxis.set_major_formatter(FormatStrFormatter('%d'))
-    plt.axes().yaxis.set_minor_locator(MultipleLocator(0.5))
+    ax.xaxis.set_major_locator(MultipleLocator(1))
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.xaxis.set_minor_locator(MultipleLocator(0.25))
 
-    plt.axes().tick_params('both', length=12, width=4, which='major', top=True, right=True, direction='in', pad=6, labelsize=30)
-    plt.axes().tick_params('both', length=8, width=3, which='minor', top=True, right=True, direction='in')
+    ax.yaxis.set_major_locator(MultipleLocator(2))
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.yaxis.set_minor_locator(MultipleLocator(0.5))
 
-    plt.axes().spines['top'].set_linewidth(4)
-    plt.axes().spines['right'].set_linewidth(4)
-    plt.axes().spines['bottom'].set_linewidth(4)
-    plt.axes().spines['left'].set_linewidth(4)
+    spine_lw = 4
+    [ax.spines[axis].set_linewidth(spine_lw) for axis in ['top', 'bottom', 'left', 'right']]
 
-    plt.xlim([app.B_VMin - 0.1, app.B_VMax + 2.5])
-    plt.ylim([18.5 - app.A_v, 8.5 - app.A_v])
-    plt.errorbar(x, y, xerr=x_err, yerr=y_err, fmt='none', ecolor='#50a0e5', elinewidth=7)
+    ax.errorbar(x, y, xerr=x_err, yerr=y_err, fmt='none', ecolor='#50a0e5', elinewidth=7)
 
     # Overplot Be candidates
-    plt.plot(be_x, be_y, 'x', color='#ff5151', markersize=15, markeredgewidth=5, label='Be Candidates')
+    ax.plot(be_x, be_y, 'x', color='#ff5151', markersize=15, markeredgewidth=5, label='Be Candidates')
 
     # Plot threshold line if 2CD
-    if output == '2CD_' + app.phot_type or output == '2CD_' + app.phot_type + '_lowError':
-        # plt.ylim([-5 - self.app.A_r, -1 - self.app.A_r])
-        try:
-            apCorr = np.loadtxt('standards/' + date + '/' + cluster + '_aperture_corrections.dat')
-        except IOError:
-            pass
+    if plot_type == '2cd':
+        apCorr = np.loadtxt('standards/' + date + '/' + cluster + '_aperture_corrections.dat')
 
-        plt.ylim([-6.5 + apCorr[2], -4 + apCorr[2]])
+        ax.set_ylim([-6.5 + apCorr[2], -4 + apCorr[2]])
 
-        plt.axes().yaxis.set_major_locator(MultipleLocator(1))
-        plt.axes().yaxis.set_minor_locator(MultipleLocator(0.25))
+        ax.yaxis.set_major_locator(MultipleLocator(1))
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+        ax.yaxis.set_minor_locator(MultipleLocator(0.25))
 
         filename = 'output/' + cluster + '/' + date + '/thresholds_' + app.phot_type + '.dat'
-        try:
-            thresholds = np.loadtxt(filename)
-            if app.threshold_type == 'Constant':
-                file = thresholds[0]
-            elif app.threshold_type == 'Linear':
-                file = thresholds[1]
-            slope = file[0]
-            intercept = file[1]
+        thresholds = np.loadtxt(filename)
+        if app.threshold_type == 'Constant':
+            file = thresholds[0]
+        elif app.threshold_type == 'Linear':
+            file = thresholds[1]
+        slope = file[0]
+        intercept = file[1]
 
-            linex = np.array([app.B_VMin, app.B_VMax])
-            liney = slope * linex + intercept
-            plt.plot(linex, liney, '--', color='#ff5151', label='Be Threshold', linewidth=6)
-        except IOError:
-            print("\nNote: Thresholds have not been calculated or written to file yet and will not be displayed.")
+        linex = np.array([app.B_VMin, app.B_VMax])
+        liney = slope * linex + intercept
+        ax.plot(linex, liney, '--', color='#ff5151', label='Be Threshold', linewidth=6)
 
-    plt.legend(fontsize=28)
-    plt.tight_layout()
+    ax.legend()
 
     # Output
     filename = 'output/' + cluster + '/' + date + '/plots/' + output
-    plt.savefig(filename)
+    fig.savefig(filename)
 
-    plt.clf()
+    # plt.cla()
+    # plt.clf()
